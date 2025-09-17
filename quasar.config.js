b7b95f68cd97dc34ac2841030d6cb82028a974b7
somnuk2 <somnuk@mju.ac.th>
2025-09-17 13:26:08 +0700

    appId: versioncontrol2

--- quasar.config.js
+++ quasar.config.js
@@ -186,7 +186,7 @@ export default defineConfig((ctx) => {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'versioncontrol'
+        appId: 'versioncontrol2'
       }
     },
 
